541ecb56c082fe1c4b42ce31a6d253d49c50a97d
Daniel Arenhage <daniel.arenhage@gmail.com>
2015-03-28 15:30:30 +0100

    added sleep method

--- squeednote.js
+++ squeednote.js
@@ -26,3 +26,12 @@ if (Meteor.isServer) {
 }
 
 Note = new Mongo.Collection("note");
+
+/*function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+};*/
